e61c901ae49b70b0f4a0999be25e41be5e198499
daniel lopez <daniel.l.m.2442.123@gmail.com>
2018-05-15 17:04:04 +0200

    Finished the Polynomial and sinusoidal regression

--- PolynomialRegression/PolynomialRegression.h
+++ PolynomialRegression/PolynomialRegression.h
@@ -2,45 +2,48 @@
 
 using namespace std;
 
-class PolynomialRegression
+template<class T>
+class NonlinearRegression
 {
 public:
-	PolynomialRegression();
-	~PolynomialRegression();
+	NonlinearRegression<T>();
+	~NonlinearRegression<T>();
 
-	template<class T>
-	static Vector<T> SPolynomialRegression(const vector<vector<T>>&,const int &,const vector<T>&,const vector<T>&);
+	static Vector<T> PolynomialRegression(const vector<vector<T>>&,const int &); // y = a0 + a1 * x + a2 * x^2 + ... + an * x^n
+	static Vector<T> SinusoidalRegression(const vector<vector<T>>&, const int &); // y = a0 + a1 * sin(2^(-n/2)*x) + a2 * sin(2^(-n/2+1)*x) + ... + an * sin(2^(n/2)*x)
+	static Vector<T> MixedRegression(const vector<vector<T>>&, const int &, const vector<T>&); // To be continued...
 
 private:
 
-	void Polynominal() {  }
-	void Sinoidal() {  }
-	void Logarithimic() {  };
+	T Polynominal(const T var,const int&n) { return pow(var, n); }
+	T Sinoidal(const T var,const int&n) {  return sin(var*pow(2, n)); }
+	T Logarithimic(const T var,const int &n) { return 5; };
 
-	typedef void (PolynomialRegression::*fptr)();
+	typedef T (NonlinearRegression::*fptr)(const T,const int&);
 	
 public:
-	fptr func[3] = { &PolynomialRegression::Polynominal, &PolynomialRegression::Sinoidal, &PolynomialRegression::Logarithimic };
+	fptr func[3] = { &NonlinearRegression::Polynominal, &NonlinearRegression::Sinoidal, &NonlinearRegression::Logarithimic };
 
 };
 
-PolynomialRegression::PolynomialRegression()
+template<class T>
+NonlinearRegression<T>::NonlinearRegression()
 {
 }
-
-PolynomialRegression::~PolynomialRegression()
-{
-}
-
 
 template<class T>
-static Vector<T> PolynomialRegression::SPolynomialRegression(
-	typename const vector<vector<T>>	 &Pts,			// the points we use
-	const int							 &order,		// the coefficient we pretend to generate
-	typename const vector<T>			 &coeffs,		// TODO
-	typename const vector<T>			 &operation)	// TODO
+NonlinearRegression<T>::~NonlinearRegression()
 {
-	Vector<double> A(order);
+}
+
+
+template<typename T>
+inline Vector<T> NonlinearRegression<T>::PolynomialRegression(
+	const vector<vector<T>>		 &Pts,			// the points we use
+	const int					 &order)		// the coefficient we pretend to generate
+{
+	// Calculate the half part of A (Matrix)
+	Vector<T> A(order);
 	for (int i = 0; i < order; i++) {
 		for (int j = i; j < order; j++) {
 			for (int k = 0; k < Pts.size();k++) {
@@ -50,16 +53,46 @@ static Vector<T> PolynomialRegression::SPolynomialRegression(
 		}
 	}
 
-	Vector<double>C(order,1);
+	// Pivotate the Matrix to fill the rest part
+	Vector<T>C(order,1);
 	for (int i = 0; i < order; i++) {
 		for (int k = 0; k < Pts.size(); k++) {
-
 			C.v[i][0] += (Pts[k][1] * pow(Pts[k][0], i));
 		}
 	}
 
-	A.Inverse();
+	// A*B=C --> B = A.inverse() * C
+	A.Inverse2();
 	A*=C;
 	return A;
 }
 
+template<class T>
+inline Vector<T> NonlinearRegression<T>::SinusoidalRegression(
+	const vector<vector<T>>		&Pts,		// the points we use
+	const int					&order)		// the sinusoidal max coefficient
+{
+	Vector<T> A(order);
+	int norder = order / 2;
+	for (int i = 0; i < order; i++) {
+		for (int j = i; j < order; j++) {
+			for (int k = 0; k < Pts.size(); k++) {
+				A.v[i][j] += sin(Pts[k][0]* pow(2,i-norder))*sin(Pts[k][0]* pow(2,j-order));
+			}
+			A.v[j][i] = A.v[i][j];
+		}
+	}
+
+	// Pivotate the Matrix to fill the rest part
+	Vector<T>C(order, 1);
+	for (int i = 0; i < order; i++) {
+		for (int k = 0; k < Pts.size(); k++) {
+			C.v[i][0] += (Pts[k][1] * pow(Pts[k][0], i));
+		}
+	}
+
+	// A*B=C --> B = A.inverse() * C
+	A.Inverse2();
+	A *= C;
+	return A;
+}
--- PolynomialRegression/main.cpp
+++ PolynomialRegression/main.cpp
@@ -1,13 +1,18 @@
 #include "../../Vector2/Vector/Vector.h"
 #include "PolynomialRegression.h"
-
+#include <ctime>
 
 int main() {
 	Vector<double> Pts;Pts.v = { { 0,2.1 },{ 1,7.7 },{ 2,12.6 },{ 3,27.2 },{ 4,40.9 },{ 5,61.1 } };
-	Vector<double> Pts; Pts.v = { { 0,2.1 },{ 1,7.7 },{ 2,12.6 },{ 3,27.2 },{ 4,40.9 },{ 5,61.1 } };
-	vector<vector<double>> e = { { 1,1,-1 },{ 3,2,1 },{ 5,3,4 } };
-	vector<double> e2 = { 1,1,2 };
-	Vector<double>dani=PolynomialRegression::SPolynomialRegression(Pts.v, 4, e2, e2);
+	Vector<double> Pts2; Pts2.v = { { 0,0 },{ 2,215 },{ 4,394 },{ 6,553 },{ 8,639 },{ 10,701 },{12,740},{14,729},{16,653},{18,584},{20,430} };
+
+	Vector<double>dani;
+	clock_t start = clock();
+	for (int i = 0; i < 1000; i++) {
+		dani = NonlinearRegression<double>::PolynomialRegression(Pts2.v, 10);
+	}
+	double time = clock() - start;
+	cout << time<<endl;
 	dani.Print();
 	getchar();
 }
